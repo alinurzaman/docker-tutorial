1022241ccac0b5eff10f94269e537a6fc9b98b00
Muhamad Ali Nur Zaman <muhamadalinurzaman@gmail.com>
2023-03-16 18:45:58 +0700

    basic compose

---
+++ 3-compose/script.sh
@@ -0,0 +1,18 @@
+# check docker compose
+docker compose version
+
+# membuat container
+# harus pada folder dimana docker-compose.yaml disimpan
+docker compose create
+
+# menjalankan container
+docker compose start
+
+# melihat list yang jalan di compose
+docker compose ps
+
+# stop compose
+docker compose stop
+
+# delete container di compose
+docker compose down
